d8da71ca67853994a7c0705e31728b3c2a6e24d4
Yurino Oga <yurino.o@latona.io>
2022-11-30 23:22:13 +0900

    release

--- README.md
+++ README.md
@@ -1,22 +1,19 @@
-# data-platform-payment-requisition-zenginkyo-sql 
-data-platform-payment-requisition-zenginkyo-sqlは、データ連携基盤において全銀協規定フォーマットの支払依頼データを維持管理するSQLテーブルを作成するためのレポジトリです。  
+# data-platform-payment-requisition-s-zedi-sql 
+data-platform-payment-requisition-s-zedi-sqlは、データ連携基盤においてS-ZEDIフォーマットの支払依頼データを維持管理するSQLテーブルを作成するためのレポジトリです。  
 
-## 全銀協規定フォーマットとは
-全銀協規定フォーマットとは、全国銀行協会連合会がデータ伝送を行うために定められたフォーマットです。
-当該組合法人インターネットバンキング総合・給与・賞与振込で、外部データから振込データを取込む際に用いられます。
+## ZEDIとは
+ZEDI（全銀EDIシステム）とは、支払企業から受け取り企業に総合振込を行う際に、EDI情報を添付できるようにするシステムです。
+総合振込のデータ形式がXML形式に移項したことにより、より多くの情報を設定でき、企業の資金決済事務の合理化が可能になります。
 
 ## 前提条件  
-data-platform-payment-requisition-zenginkyo-sqlは、データ連携にあたり、API を利用します。  
+data-platform-payment-requisition-s-zedi-sqlは、データ連携にあたり、API を利用します。  
 https://api.XXXXX.com/api/XXXXX/overview  
 本レポジトリ の sql設定ファイルの内容は、上記URL の API 仕様を前提としています。  
 
 ## sqlの設定ファイル
-data-platform-payment-requisition-zenginkyo-sqlには、sqlの設定ファイルとして以下のsqlファイルが含まれています。
+data-platform-payment-requisition-s-zedi-sqlには、sqlの設定ファイルとして以下のsqlファイルが含まれています。
 
-* data-platform-payment-requisition-zenginkyo-sql-header-record-data.sql （データ連携基盤 全銀協支払依頼 - ヘッダレコードデータ）
-* data-platform-payment-requisition-zenginkyo-sql-data-record-data.sql （データ連携基盤 全銀協支払依頼 - データレコードデータ）
-* data-platform-payment-requisition-zenginkyo-sql-trailer-record-data.sql（データ連携基盤 全銀協支払依頼 - トレーラーレコードデータ）  
-* data-platform-payment-requisition-zenginkyo-sql-end-record-data.sql（データ連携基盤 全銀協支払依頼 - エンドレコードデータ）  
+* data-platform-payment-requisition-s-zedi-sql-s-zedi-data.sql （データ連携基盤 支払依頼 S-ZEDI - S-ZEDIデータ）
 
 ## MySQLのセットアップ / Kubernetesの設定 / SQLテーブルの作成方法
 MySQLのセットアップ / Kubernetesの設定 / 具体的なSQLテーブルの作成方法、については、[mysql-kube](https://github.com/latonaio/mysql-kube)を参照ください。
---
+++ data-platform-payment-requisition-s-zedi-sql-s-zedi-data.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `data_platform_payment_requisition_s_zedi_s_zedi_data`
+(
+  `Payer`                          int(12) NOT NULL,
+  `PayerPaymentDate`               date NOT NULL,
+  `PayerPaymentRequisitionID`      int(6) NOT NULL,
+  `PayerPaymentRequisitionItem`    int(6) NOT NULL,
+  `Payee`                          int(12) NOT NULL,
+  `Buyer`                          int(12) NOT NULL,
+  `SubsetSpecifiedID`              varchar(3) NOT NULL,        -- "Z01"            業界区分
+  `BusinessProcessSpecifiedID`     varchar(3) NOT NULL,        -- "001"            データ区分
+  `ExchangedDocumentID`            varchar(40) DEFAULT NULL,   -- "123456-01"      支払通知番号
+  `IssueDateTime`                  varchar(8) NOT NULL,        -- YYYYMMDD         支払通知発行日
+  `IssuerAssignedID`               varchar(40) NOT NULL,       -- "A-123456"       請求書番号
+  `PayerID`                        varchar(13) NOT NULL,       -- 1234567890123    支払人企業法人コード 
+  `PayeeID`                        varchar(13) NOT NULL,       -- 3456789012345    受取人企業法人コード 
+  `BuyerName`                      varchar(40) NOT NULL,       -- "全銀商事"       請求先企業名
+  `BuyerID`                        int(13) NOT NULL,           -- 3456789012345    請求先企業法人コード 
+  `PaymentTotalAmount`             varchar(10) NOT NULL,       -- "10800"          支払金額（明細）
+  `BalanceOutReasonDescription`    varchar(40) DEFAULT NULL,   -- "無し"           金額相殺理由
+  `BalanceOutCalculatedAmount`     varchar(10) DEFAULT NULL,   -- "0"              相殺金額
+  `TaxCalculatedAmount1`           varchar(10) NOT NULL,       -- "800"            税額1
+  `TaxCalculatedRate1`             varchar(3) NOT NULL,        -- "8"              税率1
+  `TaxCalculatedAmount2`           varchar(10) DEFAULT NULL,   -- "1000"           税額2
+  `TaxCalculatedRate2`             varchar(3) DEFAULT NULL,    -- "10"             税率2
+  `TaxTotalAmount`                 varchar(10) NOT NULL,       -- "1800"           税額（合計）
+  `Content`                        varchar(140) DEFAULT NULL,  -- "サンプルデータ"  備考
+
+    PRIMARY KEY (`Payer`, `PayerPaymentDate`, `PayerPaymentRequisitionID`, `PayerPaymentRequisitionItem`),
+    
+    CONSTRAINT `DataPlatformPaymentRequisitionSZEDISZEDIDataPayer_fk` FOREIGN KEY (`Payer`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
+    CONSTRAINT `DataPlatformPaymentRequisitionSZEDISZEDIDataPayee_fk` FOREIGN KEY (`Payee`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
+    CONSTRAINT `DataPlatformPaymentRequisitionSZEDISZEDIDataBuyer_fk` FOREIGN KEY (`Buyer`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`)
+
+) ENGINE = InnoDB
+DEFAULT CHARSET = utf8mb4;
